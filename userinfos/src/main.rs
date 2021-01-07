9b729d041c2aeec8d4a87f776d4b38ec4fd928e1
Rrogntudju <rrogntudju@example.com>
2021-01-07 14:06:41 -0500

    OK

--- userinfos/src/main.rs
+++ userinfos/src/main.rs
@@ -1,6 +1,6 @@
-use druid::{im::{Vector}, widget::LabelText};
-use druid::widget::{Button, CrossAxisAlignment, Flex, Image, Label, List, Scroll, RadioGroup};
-use druid::{AppLauncher, Color, Data, ImageBuf, Lens, UnitPoint, Widget, WidgetExt, WindowDesc};
+use druid::im::Vector;
+use druid::widget::{Button, CrossAxisAlignment, Flex, Image, Label, List, MainAxisAlignment, RadioGroup, Scroll};
+use druid::{AppLauncher, Color, Data, ImageBuf, Lens, Widget, WidgetExt, WindowDesc};
 
 #[derive(Clone, Data, Lens)]
 struct AppData {
@@ -22,62 +22,77 @@ struct Info {
 }
 
 fn ui_builder() -> impl Widget<AppData> {
-    let mut oidc = Flex::column();
+    let mut oidc = Flex::column()
+        .must_fill_main_axis(true)
+        .cross_axis_alignment(CrossAxisAlignment::Start)
+        .main_axis_alignment(MainAxisAlignment::Center);
 
     let png_data = ImageBuf::from_data(include_bytes!("openid-icon-100x100.png")).unwrap();
     oidc.add_child(Image::new(png_data.clone()));
-    let mut titre = Label::new("OpenID Connect");
-    titre.set_text_size(30.);
-    oidc.add_child(titre);
+    oidc.add_child(Label::new("OpenID Connect").with_text_size(25.));
     oidc.add_default_spacer();
 
     oidc.add_child(Label::new("Fournisseur:"));
+    oidc.add_default_spacer();
     let mut fournisseurs = Vector::new();
     fournisseurs.push_back(("Microsoft".to_string(), Fournisseur::Microsoft));
     fournisseurs.push_back(("Google".to_string(), Fournisseur::Google));
-    oidc.add_child(RadioGroup::new(fournisseurs).lens(AppData::fournisseur));
+    oidc.add_child(
+        RadioGroup::new(fournisseurs)
+            .border(Color::rgb(1., 1., 1.), 0.3)
+            .lens(AppData::fournisseur),
+    );
+    oidc.add_default_spacer();
 
     oidc.add_child(
         Button::new("UserInfos")
             .on_click(|_, data: &mut AppData, _| {
-               data.erreur = "LOOOOOOOOOOOOOOOOOOOOOOOOOOOOOL".into();
+                data.erreur = "LOOOO OOOOOOOOO OOOOOOOOO OOOOOOOL".into();
             })
-            .fix_height(30.0)
+            .fix_height(30.0),
     );
 
-    oidc.add_child(Label::new(|data: &AppData, _env: &_| data.erreur.clone()));
+    let mut infos = Flex::column()
+        .must_fill_main_axis(true)
+        .cross_axis_alignment(CrossAxisAlignment::Start)
+        .main_axis_alignment(MainAxisAlignment::Center);
+    
+    infos.add_child(Label::new(|data: &AppData, _env: &_| {
+        let f = match data.fournisseur {
+            Fournisseur::Microsoft => "Microsoft",
+            Fournisseur::Google => "Google",
+        };
+        format!("UserInfos {}", f)
+    }));
 
-    let mut infos = Flex::column();
     infos.add_child(
-        Label::new(|data: &AppData, _env: &_| {
-            let f = match data.fournisseur {
-                Fournisseur::Microsoft => "Microsoft",
-                Fournisseur::Google => "Google",
-            };
-            format!("UserInfos {}", f)
-        })
+        Scroll::new(List::new(|| Label::new(|info: &Info, _env: &_| format!("{}    {}", info.valeur, info.propriete))).with_spacing(10.))
+            .vertical()
+            .lens(AppData::infos),
     );
 
-    infos.add_flex_child(
-        Scroll::new(
-            List::new(|| {
-                    Label::new(|info: &Info,  _env: &_| format!("{}    {}", info.valeur, info.propriete))
-            })
-            .with_spacing(10.),
+    let main = Flex::row()
+        .with_spacer(20.)
+        .with_child(oidc)
+        .with_spacer(40.)
+        .with_child(infos);
+
+    Flex::column()
+        .cross_axis_alignment(CrossAxisAlignment::Start)
+        .with_child(main)
+        .with_default_spacer()
+        .with_child(
+            Label::new(|data: &AppData, _env: &_| data.erreur.clone())
+                .with_text_color(Color::rgb(1., 0., 0.))
+                .expand_width()
         )
-        .vertical()
-        .lens(AppData::infos),
-        1.0,
-    );
 
-    let root = Flex::row().cross_axis_alignment(CrossAxisAlignment::Start);
-    root.with_spacer(20.).with_child(oidc).with_spacer(40.).with_child(infos).debug_paint_layout()
 }
 
 pub fn main() {
     let main_window = WindowDesc::new(ui_builder).title("UserInfos");
     let mut infos = Vector::new();
-    let info  = Info {
+    let info = Info {
         valeur: "Name".to_string(),
         propriete: "LOOOOOOOOOOOOOOOOOOOOOOOOOL!".to_string(),
     };
@@ -87,8 +102,5 @@ pub fn main() {
         erreur: String::new(),
         infos,
     };
-    AppLauncher::with_window(main_window)
-        .launch(data)
-        .expect("launch failed");
+    AppLauncher::with_window(main_window).launch(data).expect("launch failed");
 }
-
