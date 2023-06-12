423a278f4ed66f5c6f956def2ec60b4ceac86238
Rrogntudju <rrogntudju@example.com>
2023-06-12 18:33:40 -0400

    wip

--- crates/iced/src/mode_couleur.rs
+++ crates/iced/src/mode_couleur.rs
@@ -1,11 +1,33 @@
 use anyhow::Result;
 use iced_native::Subscription;
+use windows::Foundation::{EventRegistrationToken, TypedEventHandler};
 use windows::UI::ViewManagement::{UIColorType, UISettings};
 
 #[derive(Debug, Clone)]
 pub enum ModeCouleur {
     Claire,
     Sombre,
+}
+
+struct EventModeCouleur {
+    token: EventRegistrationToken,
+    settings: UISettings,
+}
+
+impl EventModeCouleur {
+    fn new() -> Self {
+        let settings = UISettings::new().unwrap();
+        let f = |settings, _| Ok(());
+        let handler = TypedEventHandler::new(f);
+        let token = settings.ColorValuesChanged(&handler).unwrap();
+        Self { token, settings }
+    }
+}
+
+impl Drop for EventModeCouleur {
+    fn drop(&mut self) {
+        self.settings.RemoveColorValuesChanged(self.token);
+    }
 }
 
 #[inline]
@@ -20,6 +42,10 @@ pub fn mode_couleur() -> Result<ModeCouleur> {
     } else {
         ModeCouleur::Sombre
     })
+}
+
+fn event_mod_couleur() -> Result<EventRegistrationToken> {
+    let settings = UISettings::new();
 }
 
 pub fn stream_event_mode_couleur() -> Subscription<ModeCouleur> {
