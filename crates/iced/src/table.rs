dd23c2396c562b2364ea9c9a7f65588a33bb0f07
Rrogntudju <rrogntudju@example.com>
2023-06-27 20:16:32 -0400

    wip

--- crates/iced/src/table.rs
+++ crates/iced/src/table.rs
@@ -4,6 +4,7 @@ use iced::advanced::{layout, Layout};
 use iced::advanced::{widget, Widget};
 use iced::widget::{column, container, row, text};
 use iced::{Element, Length, Rectangle, Size};
+use std::iter;
 
 pub struct TableData {
     pub header: Vec<String>,
@@ -22,7 +23,8 @@ impl Table {
 
 impl<'a, Message, Renderer> Widget<Message, Renderer> for Table
 where
-    Renderer: iced::advanced::Renderer,
+    Renderer: iced::advanced::Renderer + iced::advanced::text::Renderer,
+    Renderer::Theme: text::StyleSheet + container::StyleSheet,
 {
     fn width(&self) -> Length {
         Length::Shrink
@@ -33,7 +35,7 @@ where
     }
 
     fn layout(&self, renderer: &Renderer, limits: &layout::Limits) -> layout::Node {
-        layout::Node::new(Size::ZERO)
+        layout::Node::new(limits.max())
     }
 
     fn draw(
@@ -42,15 +44,34 @@ where
         renderer: &mut Renderer,
         theme: &Renderer::Theme,
         style: &renderer::Style,
-        _layout: Layout<'_>,
+        layout: Layout<'_>,
         cursor: mouse::Cursor,
         viewport: &Rectangle,
     ) {
-
+        //let layout = create_table(self.data, None).as_widget().layout(renderer, layout.bounds().w);
+        let count = self.data
+        .rows
+        .iter()
+        .chain(iter::once(&self.data.header))
+        .fold(vec![0; self.data.header.len()], |acc, row| {
+            acc.iter()
+                .zip(row.iter())
+                .map(|(max, s)| {
+                    let text = Into::<Element<'a, Message, Renderer>>::into(text(s));
+                    let layout = text.as_widget().layout(renderer, );
+                    let count = s.chars().count();
+                    if count > *max {
+                        count
+                    } else {
+                        *max
+                    }
+                })
+                .collect()
+        });
     }
 }
 
-fn create_table<'a, Message, Renderer>(data: &TableData, columns_max_width: Option<Vec<Length>>) -> Element<'a, Message, Renderer>
+fn create_table<'a, Message, Renderer>(data: TableData, columns_max_width: Option<Vec<Length>>) -> Element<'a, Message, Renderer>
 where
     Renderer: iced::advanced::Renderer + iced::advanced::text::Renderer,
     Renderer::Theme: text::StyleSheet + container::StyleSheet,
