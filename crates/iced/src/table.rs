a023b47b7e054d095f1c6c475926d28fd6773f7f
Rrogntudju <rrogntudju@example.com>
2023-06-28 20:01:18 -0400

    wip

--- crates/iced/src/table.rs
+++ crates/iced/src/table.rs
@@ -3,7 +3,7 @@ use iced::advanced::mouse;
 use iced::advanced::renderer;
 use iced::advanced::{layout, Layout};
 use iced::advanced::{widget, Widget};
-use iced::widget::{column, container, Row, text};
+use iced::widget::{column, container, text, Row};
 use iced::Pixels;
 use iced::{Element, Length, Rectangle, Size};
 use std::iter;
@@ -20,7 +20,10 @@ pub struct Table {
 
 impl Table {
     pub fn new(data: TableData, text_size: impl Into<Pixels>) -> Self {
-        Self { data, text_size: text_size.into().0 }
+        Self {
+            data,
+            text_size: text_size.into().0,
+        }
     }
 }
 
@@ -87,42 +90,32 @@ where
     Renderer: iced::advanced::Renderer + iced::advanced::text::Renderer,
     Renderer::Theme: text::StyleSheet + container::StyleSheet,
 {
-    let entêtes = data
+    let entête = data
         .header
         .iter()
         .zip(columns_max_width)
         .map(|(h, width)| Element::from(container(text(h).size(text_size)).width(width)))
         .collect::<Vec<_>>();
 
-    let entêtes = Row::with_children(entêtes).padding([5, 0, 5, 0]);
+    let entête = Row::with_children(entête).padding([5, 0, 5, 0]);
 
-    let infos = data
-    .rows
-    .iter()
-    .map(|row| {
-        row.iter().zip(columns_max_width)
-        .map(|(r, width)| Element::from(container(text(r).size(text_size)).width(width)))
-        .collect::<Vec<_>>()
-    }
-    )  .collect::<Vec<_>>();
-
-
-
-let infos = infos.iter().map(Row::with_children(infos).padding([5, 0, 5, 0]);
-
-    /*            let mut infos = column![];
     let mut flip = false;
+    let infos = data
+        .rows
+        .iter()
+        .map(|row| {
+            let info: Vec<Element<'_, Message, Renderer>> = row.iter()
+                .zip(columns_max_width)
+                .map(|(i, width)| {
+                    flip = !flip;
+                    Element::from(container(text(i).size(text_size)).style(style(flip)).width(width))
+                })
+                .collect::<Vec<_>>();
+            Row::with_children(info).padding([5, 0, 5, 0])
+        })
+        .collect::<Vec<_>>();
 
-    for row in &data.rows {
-        let info = row!
-            container(text(stretch(&row[0], count[0] + 1)).style(style(flip)),
-            container(text(stretch(&row[1], count[1])).size(12)).style(style(flip)),
-
-        .padding([5, 0, 0, 0]);
-        infos = infos.push(info);
-        flip = !flip;
-    }
-    column![entêtes, infos].into() */
+   column![entête, infos].into()
 }
 
 fn style(flip: bool) -> iced::theme::Container {
