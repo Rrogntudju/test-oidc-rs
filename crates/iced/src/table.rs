179b104bd6dfa0efe773e097b2f88f55feebb6a2
Rrogntudju <rrogntudju@example.com>
2023-07-03 15:58:05 -0400

    ouin

--- crates/iced/src/table.rs
+++ crates/iced/src/table.rs
@@ -17,7 +17,7 @@ pub struct TableData {
 
 pub struct Table {
     data: Vec<Vec<String>>,
-    text_size: f32,
+    text_size: Option<f32>,
 }
 
 impl Table {
@@ -33,11 +33,11 @@ impl Table {
             })
             .collect();
 
-        Self { data, text_size: 12.0 }
+        Self { data, text_size: None }
     }
 
     pub fn size(mut self, text_size: impl Into<Pixels>) -> Self {
-        self.text_size = text_size.into().0;
+        self.text_size = Some(text_size.into().0);
         self
     }
 }
@@ -74,7 +74,7 @@ where
         let columns_max_width = get_max_width::<Message, Renderer>(&self.data, self.text_size, renderer);
         let table = create_table::<Message, Renderer>(&self.data, self.text_size, &columns_max_width);
         let widget = table.as_widget();
-         let state = Tree {
+        let state = Tree {
             tag: widget.tag(),
             state: widget.state(),
             children: widget.children(),
@@ -93,7 +93,7 @@ where
     }
 }
 
-fn get_max_width<Message, Renderer>(data: &[Vec<String>], text_size: f32, renderer: &Renderer) -> Vec<f32>
+fn get_max_width<Message, Renderer>(data: &[Vec<String>], text_size: Option<f32>, renderer: &Renderer) -> Vec<f32>
 where
     Renderer: iced::advanced::Renderer + iced::advanced::text::Renderer,
     Renderer::Theme: text::StyleSheet,
@@ -103,7 +103,10 @@ where
         acc.iter()
             .zip(row.iter())
             .map(|(max, s)| {
-                let text: Element<Message, Renderer> = text(s.clone()).size(text_size).into();
+                let text: Element<Message, Renderer> = match text_size {
+                    Some(size) => text(s.clone()).size(size).into(),
+                    None => text(s.clone()).into(),
+                };
                 let layout = text.as_widget().layout(renderer, &limits);
                 let width = layout.bounds().width;
                 if width > *max {
@@ -116,7 +119,7 @@ where
     })
 }
 
-fn create_table<'a, Message, Renderer>(data: &[Vec<String>], text_size: f32, columns_max_width: &[f32]) -> Element<'a, Message, Renderer>
+fn create_table<'a, Message, Renderer>(data: &[Vec<String>], text_size: Option<f32>, columns_max_width: &[f32]) -> Element<'a, Message, Renderer>
 where
     Message: 'a,
     Renderer: iced::advanced::Renderer + iced::advanced::text::Renderer + 'a,
@@ -129,10 +132,18 @@ where
             let info: Vec<Element<Message, Renderer>> = row
                 .iter()
                 .zip(columns_max_width)
-                .map(|(i, width)| container(text(i).size(text_size)).style(style(flip)).width(*width).into())
+                .map(|(i, width)| {
+                    container(match text_size {
+                        Some(size) => text(i).size(size),
+                        None => text(i),
+                    })
+                    .style(style(flip))
+                    .width(*width)
+                    .into()
+                })
                 .collect();
             flip = !flip;
-            Row::with_children(info).padding([5, 0, 5, 0]).into()
+            Row::with_children(info).into()
         })
         .collect();
 
