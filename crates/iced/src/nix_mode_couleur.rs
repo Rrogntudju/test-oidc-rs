bb025e5bdd4119d3361d5d4a8ee636dbc8d62568
rrogntudju <you@example.com>
2023-09-14 16:24:02 -0400

    wip

--- crates/iced/src/nix_mode_couleur.rs
+++ crates/iced/src/nix_mode_couleur.rs
@@ -2,6 +2,7 @@ use anyhow::{Context, Result};
 use iced::{subscription, Subscription};
 use iced_futures::futures;
 use zbus::{dbus_proxy, zvariant::OwnedValue};
+use iced_futures::futures::StreamExt;
 
 static APPEARANCE: &str = "org.freedesktop.appearance";
 static SCHEME: &str = "color-scheme";
@@ -34,7 +35,7 @@ fn build_portal_settings_proxy<'a>() -> Result<PortalSettingsProxy<'a>> {
 }
 
 fn get_mode_couleur(value: OwnedValue) -> Result<ModeCouleur, String> {
-     match value.downcast_ref::<u32>() {
+    match value.downcast_ref::<u32>() {
         Some(1) => Ok(ModeCouleur::Sombre),
         Some(_) => Ok(ModeCouleur::Clair),
         None => Err(format!("get_mode_couleur: u32 attendu mais reçu {value:#?}")),
@@ -63,20 +64,18 @@ pub fn stream_event_mode_couleur() -> Subscription<Result<ModeCouleur, String>> 
     subscription::run_with_id(std::any::TypeId::of::<EventModeCouleurId>(), {
         futures::stream::unfold(State::Init(proxy), |state| async {
             match state {
-                State::Init(proxy) => {
-                    match proxy.Read(APPEARANCE, SCHEME).await {
-                        Ok(value) =>  {
-                            let mode = get_mode_couleur(value);
-                            match proxy.receive_SettingChanged().await {
-                                Ok(setting_changed) => Some((mode.map_err(|e| format!("{e:#}")), State::Receiving((proxy, setting_changed)))),
-                                Err(e) => Some((Err(format!("{e:#}")), State::End)),
-                            }
-                        },
-                        Err(e) => Some((Err(format!("{e:#}")), State::End)),
+                State::Init(proxy) => match proxy.Read(APPEARANCE, SCHEME).await {
+                    Ok(value) => {
+                        let mode = get_mode_couleur(value);
+                        match proxy.receive_SettingChanged().await {
+                            Ok(setting_changed) => Some((mode.map_err(|e| format!("{e:#}")), State::Receiving((proxy, setting_changed)))),
+                            Err(e) => Some((Err(format!("{e:#}")), State::End)),
+                        }
                     }
-                }
-                State::Receiving((mut rx, revoker)) => match rx.recv().await {
-                    Some(mode) => Some((mode.map_err(|e| format!("{e:#}")), State::Receiving((rx, revoker)))),
+                    Err(e) => Some((Err(format!("{e:#}")), State::End)),
+                },
+                State::Receiving((proxy, setting_changed)) => match setting_changed.next().await {
+                    Some(signal) => Some((mode.map_err(|e| format!("{e:#}")), State::Receiving((rx, revoker)))),
                     None => {
                         let erreur: Result<ModeCouleur, String> = Err("Échec du changement de mode couleur".to_string());
                         Some((erreur, State::End))
