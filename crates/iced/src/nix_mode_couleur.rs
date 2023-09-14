efef4fde6cbdfe200618ccd9f03a90a95d850413
rrogntudju <you@example.com>
2023-09-14 19:11:58 -0400

    Ok

--- crates/iced/src/nix_mode_couleur.rs
+++ crates/iced/src/nix_mode_couleur.rs
@@ -22,16 +22,12 @@ trait PortalSettings {
     fn Read(&self, namespace: &str, key: &str) -> zbus::Result<OwnedValue>;
 
     #[dbus_proxy(signal)]
-    fn SettingChanged(&self, namespace: &str, key: &str, value: OwnedValue) -> Result<()>;
+    fn SettingChanged(&self, namespace: &str, key: &str, value: OwnedValue) -> zbus::Result<()>;
 }
 
-fn build_portal_settings_proxy<'a>() -> Result<PortalSettingsProxy<'a>> {
-    let proxy = futures::executor::block_on(async {
-        let connection = zbus::ConnectionBuilder::session()?.build().await?;
-        PortalSettingsProxy::new(&connection).await.context("building proxy")
-    })?;
-
-    Ok(proxy)
+async fn build_portal_settings_proxy<'a>() -> Result<PortalSettingsProxy<'a>> {
+    let connection = zbus::ConnectionBuilder::session()?.build().await.context("build connection")?;
+    PortalSettingsProxy::new(&connection).await.context("build proxy")
 }
 
 fn get_mode_couleur(value: OwnedValue) -> Result<ModeCouleur, String> {
@@ -45,37 +41,36 @@ fn get_mode_couleur(value: OwnedValue) -> Result<ModeCouleur, String> {
 pub fn stream_event_mode_couleur() -> Subscription<Result<ModeCouleur, String>> {
     struct EventModeCouleurId;
 
-    let proxy = match build_portal_settings_proxy() {
-        Ok(proxy) => proxy,
-        Err(e) => {
-            return subscription::run_with_id(
-                std::any::TypeId::of::<EventModeCouleurId>(),
-                futures::stream::once(async move { Err(format!("{e:#}")) }),
-            )
-        }
-    };
-
     enum State<'a> {
-        Init(PortalSettingsProxy<'a>),
-        Receiving((PortalSettingsProxy<'a>, SettingChangedStream<'a>)),
+        Init(),
+        Receiving(SettingChangedStream<'a>),
         End,
     }
 
     subscription::run_with_id(std::any::TypeId::of::<EventModeCouleurId>(), {
-        futures::stream::unfold(State::Init(proxy), |state| async {
+        futures::stream::unfold(State::Init(), |state| async {
             match state {
-                State::Init(proxy) => match proxy.Read(APPEARANCE, SCHEME).await {
-                    Ok(value) => {
-                        let mode = get_mode_couleur(value);
-                        match proxy.receive_SettingChanged().await {
-                            Ok(setting_changed) => Some((mode.map_err(|e| format!("{e:#}")), State::Receiving((proxy, setting_changed)))),
-                            Err(e) => Some((Err(format!("{e:#}")), State::End)),
+                State::Init() => match build_portal_settings_proxy().await {
+                    Ok(proxy) => match proxy.Read(APPEARANCE, SCHEME).await {
+                        Ok(value) => {
+                            let mode = get_mode_couleur(value);
+                            match proxy.receive_SettingChanged().await {
+                                Ok(setting_changed) => Some((mode.map_err(|e| format!("{e:#}")), State::Receiving(setting_changed))),
+                                Err(e) => Some((Err(format!("{e:#}")), State::End)),
+                            }
                         }
-                    }
+                        Err(e) => Some((Err(format!("{e:#}")), State::End)),
+                    },
                     Err(e) => Some((Err(format!("{e:#}")), State::End)),
                 },
-                State::Receiving((proxy, setting_changed)) => match setting_changed.next().await {
-                    Some(signal) => Some((mode.map_err(|e| format!("{e:#}")), State::Receiving((rx, revoker)))),
+                State::Receiving(setting_changed) => match setting_changed.next().await {
+                    Some(signal) => match signal.args() {
+                        Ok(args) => {
+                            let mode = get_mode_couleur(*args.value());
+                            Some((mode.map_err(|e| format!("{e:#}")), State::Receiving(setting_changed)))
+                        }
+                        Err(e) => Some((Err(format!("{e:#}")), State::End)),
+                    },
                     None => {
                         let erreur: Result<ModeCouleur, String> = Err("Échec du changement de mode couleur".to_string());
                         Some((erreur, State::End))
