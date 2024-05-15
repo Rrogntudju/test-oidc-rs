f8361d2878d13b783d1f04e389e40d9bcd7744c1
Rrogntudju <rrogntudju@example.com>
2024-05-15 11:41:54 -0400

    wip

--- crates/xilem/src/main.rs
+++ crates/xilem/src/main.rs
@@ -1,13 +1,3 @@
-// Copyright 2018 the Xilem Authors and the Druid Authors
-// SPDX-License-Identifier: Apache-2.0
-
-//! Simple calculator.
-
-// On Windows platform, don't show a console when opening the app.
-#![windows_subsystem = "windows"]
-#![allow(clippy::single_match)]
-
-mod table;
 use std::sync::Arc;
 
 use accesskit::{DefaultActionVerb, Role};
@@ -22,6 +12,202 @@ use smallvec::{smallvec, SmallVec};
 use tracing::{trace, trace_span, Span};
 use winit::dpi::LogicalSize;
 use winit::window::Window;
+
+mod table;
+use serde_json::value::Value;
+use std::{fmt, thread};
+use table::{Table, TableData};
+
+mod pkce;
+use pkce::Pkce;
+
+
+const FINISH_GET_USERINFOS: Selector<Result<Vec<Vec<String>>, String>> = Selector::new("finish_get_userinfos");
+const ID_MS: &str = include_str!("../../../secrets/clientid.microsoft");
+const SECRET_MS: &str = include_str!("../../../secrets/secret.microsoft");
+const ID_GG: &str = include_str!("../../../secrets/clientid.google");
+const SECRET_GG: &str = include_str!("../../../secrets/secret.google");
+const AUTH_MS: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize";
+const AUTH_GG: &str = "https://accounts.google.com/o/oauth2/v2/auth";
+const TOKEN_MS: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/token";
+const TOKEN_GG: &str = "https://oauth2.googleapis.com/token";
+const INFOS_MS: &str = "https://graph.microsoft.com/oidc/userinfo";
+const INFOS_GG: &str = "https://openidconnect.googleapis.com/v1/userinfo";
+
+#[dynamic]
+static mut TOKEN: Option<(Fournisseur, Pkce)> = None;
+
+#[derive(Clone)]
+struct AppData {
+    radio_fournisseur: Fournisseur,
+    label_fournisseur: String,
+    infos: Arc<TableData>,
+    en_traitement: bool,
+    erreur: String,
+}
+
+#[derive(Clone, PartialEq)]
+pub enum Fournisseur {
+    Microsoft,
+    Google,
+}
+
+impl fmt::Display for Fournisseur {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let fournisseur = match self {
+            Fournisseur::Microsoft => "Microsoft",
+            Fournisseur::Google => "Google",
+        };
+        f.write_str(fournisseur)
+    }
+}
+
+impl Fournisseur {
+    fn endpoints(&self) -> (&str, &str) {
+        match self {
+            Self::Microsoft => (AUTH_MS, TOKEN_MS),
+            Self::Google => (AUTH_GG, TOKEN_GG),
+        }
+    }
+
+    fn secrets(&self) -> (&str, &str) {
+        match self {
+            Self::Microsoft => (ID_MS, SECRET_MS),
+            Self::Google => (ID_GG, SECRET_GG),
+        }
+    }
+
+    fn userinfos(&self) -> &str {
+        match self {
+            Self::Microsoft => INFOS_MS,
+            Self::Google => INFOS_GG,
+        }
+    }
+}
+
+fn request_userinfos(f: &Fournisseur) -> Result<Value, anyhow::Error> {
+    let token = TOKEN.read();
+    if token.is_some() {
+        let (fournisseur, secret) = token.as_ref().unwrap();
+        if f != fournisseur || secret.is_expired() {
+            drop(token);
+            TOKEN.write().replace((f.to_owned(), Pkce::new(f)?));
+        }
+    } else {
+        drop(token);
+        TOKEN.write().replace((f.to_owned(), Pkce::new(f)?));
+    }
+
+    Ok(ureq::get(f.userinfos())
+        .set("Authorization", &format!("Bearer {}", TOKEN.read().as_ref().unwrap().1.secret()))
+        .call()?
+        .into_json::<Value>()?)
+}
+
+fn get_userinfos(sink: ExtEventSink, fournisseur: Fournisseur) {
+    thread::spawn(move || {
+        let result = match request_userinfos(&fournisseur) {
+            Ok(value) => match value {
+                Value::Object(map) => {
+                    let table: Vec<Vec<String>> = map.iter().map(|(k, v)| vec![k.to_owned(), v.to_string().replace('"', "")]).collect();
+                    Ok(table)
+                }
+                _ => Err("La valeur doit être un map".to_string()),
+            },
+            Err(e) => Err(e.to_string()),
+        };
+
+        sink.submit_command(FINISH_GET_USERINFOS, result, Target::Auto)
+            .expect("command failed to submit");
+    });
+}
+
+struct Delegate;
+
+impl AppDelegate<AppData> for Delegate {
+    fn command(&mut self, _ctx: &mut DelegateCtx, _target: Target, cmd: &Command, data: &mut AppData, _env: &Env) -> Handled {
+        match cmd.get(FINISH_GET_USERINFOS) {
+            Some(Ok(infos)) => {
+                data.en_traitement = false;
+                data.infos = Arc::new(TableData {
+                    rows: infos.to_owned(),
+                    header: vec!["Propriété".to_owned(), "Valeur".to_owned()],
+                });
+                Handled::Yes
+            }
+            Some(Err(e)) => {
+                data.en_traitement = false;
+                data.erreur = e.to_owned();
+                Handled::Yes
+            }
+            None => Handled::No,
+        }
+    }
+}
+
+fn ui_builder() -> impl Widget {
+    let mut oidc = Flex::column()
+        .must_fill_main_axis(true)
+        .cross_axis_alignment(CrossAxisAlignment::Start)
+        .main_axis_alignment(MainAxisAlignment::Center);
+
+    let png_data = ImageBuf::from_data(include_bytes!("openid-icon-100x100.png")).unwrap();
+    oidc.add_child(Image::new(png_data));
+    oidc.add_child(Label::new("OpenID Connect").with_text_size(25.));
+    oidc.add_default_spacer();
+
+    oidc.add_child(Label::new("Fournisseur:"));
+    oidc.add_default_spacer();
+    let mut fournisseurs = Vector::new();
+    fournisseurs.push_back((Fournisseur::Microsoft.to_string(), Fournisseur::Microsoft));
+    fournisseurs.push_back((Fournisseur::Google.to_string(), Fournisseur::Google));
+    oidc.add_child(RadioGroup::row(fournisseurs).lens(AppData::radio_fournisseur));
+    oidc.add_default_spacer();
+
+    let bouton = Button::new("UserInfos")
+        .on_click(|ctx, data: &mut AppData, _| {
+            data.erreur = String::new();
+            data.label_fournisseur = data.radio_fournisseur.to_string();
+            data.en_traitement = true;
+            get_userinfos(ctx.get_external_handle(), data.radio_fournisseur.clone());
+        })
+        .fix_height(30.0);
+
+    oidc.add_child(Either::new(|data, _env| data.en_traitement, Spinner::new(), bouton));
+
+    let infos = Flex::column()
+        .must_fill_main_axis(true)
+        .cross_axis_alignment(CrossAxisAlignment::Start)
+        .main_axis_alignment(MainAxisAlignment::Center)
+        .with_child(
+            Label::new(|data: &AppData, _env: &_| format!("UserInfos {}", data.label_fournisseur))
+                .with_text_size(18.)
+                .with_text_color(Color::from_hex_str("FFA500").unwrap()),
+        )
+        .with_default_spacer()
+        .with_child(
+            Table::new()
+                .with_header_text_color(Color::from_hex_str("FFA500").unwrap())
+                .lens(AppData::infos),
+        );
+
+    let main = Flex::row().with_default_spacer().with_child(oidc).with_spacer(40.).with_child(infos);
+
+    Flex::column()
+        .cross_axis_alignment(CrossAxisAlignment::Start)
+        .with_child(main)
+        .with_default_spacer()
+        .with_child(
+            Flex::row().with_default_spacer().with_child(
+                Label::new(|data: &AppData, _env: &_| data.erreur.clone())
+                    .with_text_color(Color::rgb(1., 0., 0.))
+                    .expand_width(),
+            ),
+        )
+    //    .debug_paint_layout()
+}
+
+
 
 #[derive(Clone)]
 struct CalcState {
---
+++ crates/xilem/src/pkce.rs
@@ -0,0 +1,137 @@
+use crate::Fournisseur;
+use anyhow::{anyhow, Context, Error};
+use oauth2::basic::BasicClient;
+use oauth2::ureq::http_client;
+use oauth2::{
+    AccessToken, AuthType, AuthUrl, AuthorizationCode, ClientId, ClientSecret, CsrfToken, PkceCodeChallenge, RedirectUrl, Scope, TokenResponse,
+    TokenUrl,
+};
+use std::io::{BufRead, BufReader, Write};
+use std::net::TcpListener;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::mpsc::{sync_channel, Receiver};
+use std::sync::Arc;
+use std::time::{Duration, Instant};
+use tokio::task::spawn_blocking;
+use url::Url;
+
+#[derive(Debug, Clone)]
+pub struct Pkce {
+    token: AccessToken,
+    creation: Instant,
+    expired_in: Duration,
+}
+
+impl Pkce {
+    pub async fn new(f: &Fournisseur) -> Result<Self, Error> {
+        let (id, secret) = f.secrets();
+        let id = ClientId::new(id.to_owned());
+        let secret = ClientSecret::new(secret.to_owned());
+
+        let (url_auth, url_token) = f.endpoints();
+        let url_auth = AuthUrl::new(url_auth.to_owned())?;
+        let url_token = TokenUrl::new(url_token.to_owned())?;
+
+        let client = BasicClient::new(id, Some(secret), url_auth, Some(url_token))
+            .set_auth_type(AuthType::RequestBody)
+            .set_redirect_uri(RedirectUrl::new("http://localhost:86".to_owned())?);
+
+        let (pkce_code_challenge, pkce_code_verifier) = PkceCodeChallenge::new_random_sha256();
+
+        let (authorize_url, csrf) = client
+            .authorize_url(CsrfToken::new_random)
+            .add_scope(Scope::new("openid".to_owned()))
+            .add_scope(Scope::new("email".to_owned()))
+            .add_scope(Scope::new("profile".to_owned()))
+            .set_pkce_challenge(pkce_code_challenge)
+            .url();
+
+        let listener = TcpListener::bind("[::1]:86").context("TCP bind")?;
+        let (rx, stop_signal) = start_listening(listener, csrf)?;
+        if let Err(e) = webbrowser::open(authorize_url.as_ref()).context("open browser") {
+            stop_signal.store(true, Ordering::Relaxed);
+            return Err(e);
+        }
+
+        let code = spawn_blocking(move || match rx.recv() {
+            Ok(code) => Ok(code),
+            Err(_) => Err(anyhow!("Vous devez vous authentifier")),
+        })
+        .await??;
+
+        let creation = Instant::now();
+        let token = client.exchange_code(code).set_pkce_verifier(pkce_code_verifier).request(http_client)?;
+        let expired_in = token.expires_in().unwrap_or(Duration::from_secs(3600));
+        let token = token.access_token().to_owned();
+        Ok(Self { token, creation, expired_in })
+    }
+
+    pub fn is_expired(&self) -> bool {
+        self.creation.elapsed() >= self.expired_in
+    }
+
+    pub fn secret(&self) -> &String {
+        self.token.secret()
+    }
+}
+
+fn start_listening(listener: TcpListener, csrf: CsrfToken) -> Result<(Receiver<AuthorizationCode>, Arc<AtomicBool>), Error> {
+    let (tx, rx) = sync_channel::<AuthorizationCode>(1);
+    let stop_signal = Arc::new(AtomicBool::new(false));
+    let stop_signal2 = stop_signal.clone();
+    listener.set_nonblocking(true).expect("Erreur set_nonblocking");
+
+    std::thread::spawn(move || {
+        let now = Instant::now();
+        while !stop_signal2.load(Ordering::Relaxed) {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut request_line = String::new();
+                    let mut reader = BufReader::new(&stream);
+                    reader.read_line(&mut request_line).unwrap();
+
+                    let redirect_url = request_line.split_whitespace().nth(1).unwrap();
+                    let url = Url::parse(&(format!("http://localhost{redirect_url}"))).unwrap();
+                    let code_pair = url
+                        .query_pairs()
+                        .find(|pair| {
+                            let (key, _) = pair;
+                            key == "code"
+                        })
+                        .expect("Le code d'autorisation doit être présent");
+
+                    let (_, value) = code_pair;
+                    let code = AuthorizationCode::new(value.into_owned());
+
+                    let state_pair = url
+                        .query_pairs()
+                        .find(|pair| {
+                            let (key, _) = pair;
+                            key == "state"
+                        })
+                        .expect("Le jeton csrf doit être présent");
+
+                    let (_, value) = state_pair;
+                    assert_eq!(csrf.secret(), value.as_ref());
+
+                    let message = "<p>Retournez dans l'application &#128526;</p>";
+                    let response = format!("HTTP/1.1 200 OK\r\ncontent-length: {}\r\n\r\n{message}", message.len());
+                    stream.write_all(response.as_bytes()).expect("Erreur write_all");
+
+                    let _ = tx.send(code);
+                    break;
+                }
+                Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                    if now.elapsed().as_secs() >= 150 {
+                        break;
+                    }
+                    std::thread::sleep(Duration::from_millis(100));
+                    continue;
+                }
+                Err(e) => panic!("accept IO error: {e}"),
+            }
+        }
+    });
+
+    Ok((rx, stop_signal))
+}
